7c6d3ccc0dfd977302f64c012fe48b95f9c162dd
Rodrigo Enrique Silva De Farias <VIC0247338@aluno.firjansenaisesi.com.br>
2024-05-16 16:13:03 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+     <View style={styles.segundoContainer}></View>
+     <View style={styles.terceiroContainer}></View>
+     <View style={styles.quartoContainer}></View>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#ddf',
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-evenly',
+    gap: 200,
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  
+  segundoContainer: {
+    backgroundColor: 'red',
+    flex: 1,
+    width: '10%',
+    height: '10%',
+    justifyContent: 'space-evenly',
+
+  },
+
+  terceiroContainer:{
+    backgroundColor: 'green',
+    flex: 1,
+    width: '10%',
+    height: '10%',
+    justifyContent: 'space-evenly',
+
+  },
+
+  quartoContainer:{
+    backgroundColor: 'yellow',
+    flex: 1,
+    width: '10%',
+    height: '10%',
+    justifyContent: 'space-evenly',
+
+  },
+
 });
